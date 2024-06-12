b86a4370cc0ac56408c0eeb96f10a83c28ad03e3
Nkhanh218 <exodiakhanh@gmail.com>
2024-06-12 13:57:01 +0700

    Admin dashboard:Add rooms,Delete Rooms

---
+++ resources/views/admin/create_room.blade.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+    @include('admin.css')
+    <style type="text/css">
+        label {
+            display: inline-block;
+            width: 200px;
+        }
+
+        .dev_deg {
+            padding-top: 30px;
+        }
+
+        .div_center {
+            text-align: center;
+            padding-top: 40px;
+        }
+    </style>
+</head>
+
+<body>
+    @include('admin.header')
+    @include('admin.sidebar')
+    <div class="page-content">
+        <div class="page-header">
+            <div class="container-fluid">
+                <div class="div_center">
+                    <h1 style="font-size: 30px; font-weigh:bold;">Add Rooms</h1>
+                    <form action="{{url('add_room')}}" method="Post" enctype="multipart/form-data">
+                        @csrf
+                        <div class="dev_deg">
+                            <label for="">Room Title</label>
+                            <input type="text" name="title" id="">
+                        </div>
+                        <div class="dev_deg">
+                            <label for="">Description</label>
+                            <textarea name="description" id=""></textarea>
+                        </div>
+                        <div class="dev_deg">
+                            <label for="">Price</label>
+                            <input type="number" name="price" id="">
+                        </div>
+                        <div class="dev_deg">
+                            <label for="">Room Type</label>
+                            <select name="type" id="">
+                                <option value="regular">Regular</option>
+                                <option value="premium">Premium</option>
+                                <option value="deluxe">Deluxe</option>
+
+                            </select>
+                        </div>
+                        <div class="dev_deg">
+                            <label for="">Free Wifi</label>
+                            <select name="wifi" id="">
+                                <option value="yes">yes</option>
+                                <option value="no">No</option>
+                            </select>
+                        </div>
+                        <div class="dev_deg">
+                            <label>Upload Image</label>
+                            <input type="file" name="image">
+                        </div>
+                        <div class="dev_deg">
+                            <input class="btn btn-primary" type="submit" value="Add Room">
+                        </div>
+                    </form>
+                </div>
+
+            </div>
+        </div>
+    </div>
+    @include('admin.footer')
+</body>
+
+</html>
